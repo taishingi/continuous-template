8ce037c2f5995bc4bed34b36e853da5d3b80bf69
Willy Micieli <micieli@vivaldi.net>
2023-11-26 11:47:56 +0100

    clean code

--- go/beta.sh
+++ go/beta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo stable
-sudo apt-get install git -y
+apt-get install git -y
 git clone https://github.com/taishingi/continuous-template continuous
 cd continuous/go
 go run main.go
--- go/nightly.sh
+++ go/nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git -y
+apt-get install git -y
 git clone https://github.com/taishingi/continuous-template continuous
 cd continuous/go
 go run main.go
